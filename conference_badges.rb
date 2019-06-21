ca75e4978645f55211f5cf63d524efc3f523f27f
Jordan Bacon <jmbacon92@gmail.com>
2019-06-21 00:55:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.each do |n|
+    puts badge_maker#{n}
+end
+end
+
+def assign_rooms(attendees)
+  "Hello, #{attendees[]}!You'll be assigned to room"
+  attendees.each_with_index do |value, index|
+    puts "#{index}: #{value}"
+
+ describe '#assign_rooms' do
+
+    # Question 3
+
+    it 'should return a list of welcome messages and room assignments' do
+      expect(assign_rooms(attendees)).to eq(room_assignments)
+    end
+
+  end
