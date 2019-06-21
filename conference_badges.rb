e4c1f3ff8fc760217b799b04b32a01414fb295f8
Jordan Bacon <jmbacon92@gmail.com>
2019-06-21 01:12:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  badges.each {|attendees| "Hello, my name is #{attendees}"}
+  badges.each {|attendees| "Hello, my name is #{attendees}."}
 end
 
 def assign_rooms(attendees)
