61540d032d9760308cd28ed9665a2aa0b7ee8dbd
Jordan Bacon <jmbacon92@gmail.com>
2019-06-21 01:01:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,20 +5,15 @@ end
 def batch_badge_creator(attendees)
   badges = []
   attendees.each do |n|
-  badges.push(badge_maker(n)
+    badges.push(badge_maker(n)
+  end
+  return badges
 end
-return badges
-end
-
-def assign_rooms(attendees)
-  "Hello, #{attendees[]}!You'll be assigned to room"
-  attendees.each_with_index do |value, index|
-    puts "#{index}: #{value}"
 
 def assign_rooms(attendees)
   room_assignments = []
   attendees.each_with_index do |name, number|
     room_assignments.push("Hello, #{name}! You'll be assigned to room #{number+1}!")
   end
-  room_assignments
+  return room_assignments
 end
